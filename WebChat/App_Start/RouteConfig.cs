60021bb9b1c9d50338d8cd844e0d4a8b6af4be9d
Aleksandr Pyzh <pyzh98@gmail.com>
2018-11-22 20:49:06 +0200

    Revert "models was added"
    This reverts commit 479f458848e7262534a053dd666f55afce7e76b9.

--- WebChat/App_Start/RouteConfig.cs
+++ WebChat/App_Start/RouteConfig.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace WebChat
@@ -12,7 +16,7 @@ namespace WebChat
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Chat", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
