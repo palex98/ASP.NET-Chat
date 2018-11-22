a9707c321cfca94b1eb84b2650b415cef4323fa3
Aleksandr Pyzh <pyzh98@gmail.com>
2018-11-22 17:59:59 +0200

    first commit, project was created

---
+++ WebChat/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется следующим образом
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("WebChat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebChat")]
+[assembly: AssemblyCopyright("© , 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("8c343da6-5d89-4b43-b4f8-c0b4e28d86be")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      Дополнительный номер версии
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер редакции и номер сборки по умолчанию,
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
