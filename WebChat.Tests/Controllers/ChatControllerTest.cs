6487f79d79e512ac56e862d95a637ef2bea62fc7
Aleksandr Pyzh <pyzh98@gmail.com>
2018-11-25 23:29:22 +0200

    add test

---
+++ WebChat.Tests/Controllers/ChatControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebChat.Controllers;
+
+namespace WebChat.Tests.Controllers
+{
+    [TestClass]
+    public class ChatControllerTest
+    {
+        [TestMethod]
+        public void IndexViewResultNotNull()
+        {
+            ChatController controller = new ChatController();
+
+           var result = controller.EmojiParse("any text :-)");
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
